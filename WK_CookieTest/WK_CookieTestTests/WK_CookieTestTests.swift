c4db50b0ee7f1a36c819b3aca50d9ef0f04f439c
anchoriteFili <feiliyaailiya1@ilcoud.com>
2018-06-08 15:36:24 +0800

    初始化项目

---
+++ WK_CookieTest/WK_CookieTestTests/WK_CookieTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  WK_CookieTestTests.swift
+//  WK_CookieTestTests
+//
+//  Created by zetafin on 2018/6/8.
+//  Copyright © 2018年 赵宏亚. All rights reserved.
+//
+
+import XCTest
+@testable import WK_CookieTest
+
+class WK_CookieTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
